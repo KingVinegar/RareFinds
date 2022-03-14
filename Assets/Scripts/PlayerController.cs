f559d76d63eef7256c1a52d7e2c359827779cc30
KingVinegar <35643613+KingVinegar@users.noreply.github.com>
2022-03-14 16:39:12 -0400

    Basic Systems

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,6 +8,7 @@ public class PlayerController : MonoBehaviour
 {
     private BoxCollider2D boxCollider;
     private Vector3 moveDelta;
+    private RaycastHit2D hit;
 
     // Start is called before the first frame update
     private void Start()
@@ -27,6 +28,20 @@ public class PlayerController : MonoBehaviour
         else if (moveDelta.x < 0)
             transform.localScale = new Vector3(-1, 1, 1);
 
-        transform.Translate(moveDelta * Time.deltaTime); 
+        //Check to make sure we can move by casting a box first.  If box is null, free to move
+        hit = Physics2D.BoxCast(transform.position, boxCollider.size, 0, new Vector2(0, moveDelta.y), Mathf.Abs(moveDelta.y * Time.deltaTime), LayerMask.GetMask("Actor", "Blocking"));
+        if (hit.collider == null)
+        {
+            //Movement function
+            transform.Translate(0, moveDelta.y * Time.deltaTime, 0);
+        }
+
+        //Check to make sure we can move by casing a box first.  If box is null, free to move
+        hit = Physics2D.BoxCast(transform.position, boxCollider.size, 0, new Vector2(moveDelta.x, 0), Mathf.Abs(moveDelta.x * Time.deltaTime), LayerMask.GetMask("Actor", "Blocking"));
+        if (hit.collider == null)
+        {
+            //Movement function
+            transform.Translate(moveDelta.x * Time.deltaTime, 0, 0);
+        }
     }
 }
