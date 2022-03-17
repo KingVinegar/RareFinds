9ad15657bc053ab5a06bbb6805e5b65c2fad0a30
KingVinegar <35643613+KingVinegar@users.noreply.github.com>
2022-03-17 19:55:58 -0400

    Transitions added

---
+++ Assets/Scripts/Transitioner.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Transitioner : MonoBehaviour
+{
+    public string sceneToLoad;
+
+    public void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            SceneManager.LoadScene(sceneToLoad);
+        }
+        
+        
+    }
+}
